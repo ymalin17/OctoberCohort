03fd612d9de5538cf047c1a963de45b85e8e5cae
Ymalin17 <yahyemalin@outlook.com>
2023-10-19 23:03:40 +0100

    new project content

---
+++ Lessons/JavaScript/Lesson1.js
@@ -0,0 +1,10 @@
+
+var newName ="yahye"
+var welcomeMessage ="welcome to ST Georges Hospital"
+
+
+const nelcomeMessage = "welcome to me"
+
+nelcomeMessages = "thursday"
+
+console.log (nelcomeMessages)
